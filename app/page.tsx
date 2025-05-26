7e605845d31a1bf7995ee5410277864eb6b29eb8
lou1s <lou1svuong.dev@gmail.com>
2025-05-26 11:21:42 +0700

    Update main layout to ensure full-height display

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Chat } from "@/components/ui/chat";
 import { Header } from "@/components/layouts/header";
 export default function Home() {
   return (
-    <main className="flex max-h-screen max-w-5xl mx-auto justify-center items-center flex-col">
+    <main className="flex h-screen max-w-5xl mx-auto justify-center items-center flex-col">
       <Header />
       <Chat />
     </main>
